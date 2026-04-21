a4c910cee9dc917f87cda5c60871b4c4bc947d3a
Darrin Holtz <darrinholtz@gmail.com>
2026-04-21 22:59:13 +0000

    installed 3 step cart page

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -1,7 +1,12 @@
 "use client"
 
-import { CartItemsType } from "@/types";
+import PaymentForm from "@/components/PaymentForm";
+import ShippingForm from "@/components/ShippingForm";
+import { CartItemsType, ShippingFormInputs } from "@/types";
+import { ArrowRight, Trash2 } from "lucide-react";
+import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const steps = [
   {
@@ -79,6 +84,7 @@ const steps = [
 const CartPage = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
+  const [shippingForm, setShippingForm] = useState<ShippingFormInputs | null>(null);
 
   const activeStep = parseInt(searchParams.get("step") || "1");
   return (
@@ -101,6 +107,70 @@ const CartPage = () => {
               }`}>{step.title}</p>
           </div>
         ))}
+      </div>
+      <div className="w-full flex flex-col lg:flex-row gap-16">
+        <div className="w-full lg:w-7/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
+          {activeStep === 1 ? (
+            cartItems.map(item => (
+              <div className="flex items-center justify-between" key={item.id}>
+                <div className="flex gap-8">
+                  <div className="relative w-32 h-32 bg-gray-50 rounded-lg overflow-hidden">
+                    <Image src={item.images[item.selectedColor]} alt={item.name} fill className="object-contain"/>
+                  </div>
+                  <div className="flex flex-col justify-between">
+                    <div className="flex flex-col gap-1">
+                      <p className="text-sm font-medium">{item.name}</p>
+                      <p className="text-xs text-gray-500">Quantity: {item.quantity}</p>
+                      <p className="text-xs text-gray-500">Size: {item.selectedSize.toUpperCase()}</p>
+                      <p className="text-xs text-gray-500">Color: {item.selectedColor}</p>
+                    </div>
+                    <p className="font-medium">${(item.price * item.quantity).toFixed(2)}</p>
+                  </div>
+                </div>
+                <button title="Remove item" className="w-8 h-8 rounded-full bg-red-100 text-red-400 hover:bg-red-200 transition-all duration-300 flex items-center justify-center cursor-pointer">
+                  <Trash2 className="w-3 h-3" />
+                </button>
+              </div>
+            ))
+          ) : activeStep === 2 ? (
+            <ShippingForm setShippingForm={setShippingForm} />
+          ) : (
+            activeStep === 3 && shippingForm ? <PaymentForm /> : <p className="text-sm text-gray-500">Please fill in the shipping form to continue</p>
+          )}
+        </div>
+        <div className="w-full lg:w-5/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8 h-max">
+          <h2 className="font-semibold">Cart Details</h2>
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between text-sm">
+              <p className="text-gray-500">Subtotal</p>
+              <p className="font-medium">${cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}</p>
+            </div>
+            <div className="flex justify-between text-sm">
+              <p className="text-gray-500">Discount(10%)</p>
+              <p className="font-medium text-red-500">-$ 10</p>
+            </div>          
+            <div className="flex justify-between text-sm">
+              <p className="text-gray-500">Shipping Fee</p>
+              <p className="font-medium">$10</p>
+            </div>
+            <hr className="border-gray-200" />
+            <div className="flex justify-between">
+              <p className="text-gray-800 font-semibold">Total</p>
+              <p className="font-medium">
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                  .toFixed(2)}
+              </p>
+            </div>
+          </div>
+          {activeStep === 1 && (
+            <button className="w-full bg-gray-800 hover:bg-gray-500 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2" onClick={() => router.push("/cart?step=2", { scroll: false })}>
+              Continue
+              <ArrowRight className="w-3 h-3"/>
+            </button>
+          )}
+        </div>
       </div>
     </div>
   )
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export type ProductType = {
   id: string | number;
   name: string;
@@ -18,3 +20,23 @@ export type CartItemType = ProductType & {
 };
 
 export type CartItemsType = CartItemType[];
+
+export const shippingFormSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email().min(1, "Email is required"),
+  phone: z.string().min(7, "Phone number must be at least 7 characters").max(10, "Phone number must be at most 10 characters").regex(/^\d+$/, "Phone number must contain only digits"),
+  address: z.string().min(1, "Address is required"),
+  city: z.string().min(1, "City is required"),
+  postalCode: z.string().min(1, "Postal code is required"),
+})
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder name is required"),
+  cardNumber: z.string().min(16, "Card number must be at least 16 characters").max(19, "Card number must be at most 19 characters").regex(/^\d+$/, "Card number must contain only digits"),
+  expirationDate: z.string().min(5, "Expiration date must be in MM/YY format").max(5, "Expiration date must be in MM/YY format").regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Expiration date must be in MM/YY format"),
+  cvv: z.string().min(3, "CVV must be at least 3 characters").max(4, "CVV must be at most 4 characters").regex(/^\d+$/, "CVV must contain only digits"),
+})
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
