a27b11f75baa9b39b8c5e5b5aeba3e4c0ac6b8bf
Darrin Holtz <darrinholtz@gmail.com>
2026-04-22 12:30:20 +0000

    completed cart functions

--- client/src/components/ShoppingCartIcon.tsx
+++ client/src/components/ShoppingCartIcon.tsx
@@ -2,12 +2,16 @@
 
 import Link from "next/link"
 import { ShoppingCart } from "lucide-react"
+import CartPage from "@/app/cart/page"
+import useCartStore from "@/stores/cartStore"
 
 const ShoppingCartIcon = () => {
+  const { cart, hasHydrated } = useCartStore();
+  if (!hasHydrated) return null;
   return (
     <Link href="/cart" title="Cart" className="relative">
         <ShoppingCart className="w-4 h-4 text-gray-600"/>
-        <span className="absolute -top-3 -right-2 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">2</span>
+        <span className="absolute -top-3 -right-2 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">{cart.reduce((acc, item) => acc + item.quantity, 0)}</span>
     </Link>
   )
 }
