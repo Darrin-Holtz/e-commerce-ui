03544420d4daee840d8898c5a71e4be0cf20f400
Darrin Holtz <darrinholtz@gmail.com>
2026-04-29 20:39:36 +0000

    finished setting up frontend with backend

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const allowedOrigins = process.env.CODESPACE_NAME
+  ? [
+      `${process.env.CODESPACE_NAME}-3001.app.github.dev`,
+      "localhost:3001",
+    ]
+  : [];
+
 const nextConfig: NextConfig = {
   transpilePackages: ["@e-commerce-ui/types", "@e-commerce-ui/ui"],
+  experimental: {
+    serverActions: {
+      allowedOrigins,
+    },
+  },
   images:{
     remotePatterns:[
       {
